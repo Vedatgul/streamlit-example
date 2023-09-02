8e01eb1c88783496b358f95ce9cc6ab71191ada4
Vedat Gül <124357663+Vedatgul@users.noreply.github.com>
2023-09-02 13:03:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,28 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-"""
-# Welcome to Streamlit!
+# Kullanıcıdan müşteri sayısını alın
+num_customers = st.number_input("Müşteri Sayısını Girin", min_value=1, step=1)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Müşterilerin maaşlarını girmek için bir liste oluşturun
+salaries = []
+for i in range(num_customers):
+    salary = st.number_input(f"{i+1}. Müşteri Maaşı", min_value=0, step=1)
+    salaries.append(salary)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Grafik oluşturma işlemini gerçekleştirin
+if st.button("Grafikleri Oluştur"):
+    # Verileri bir veri çerçevesine aktarın
+    data = {'Maaş': salaries}
+    df = pd.DataFrame(data)
+    
+    # Aynı maaşı alanları sayın
+    maas_sayisi = df['Maaş'].value_counts()
+    
+    # Grafiği oluşturun
+    st.bar_chart(maas_sayisi)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # Çubukların üzerine değerleri yazdırın
+    for i, v in enumerate(maas_sayisi):
+        st.text(f"{i}: {v} kişi")
