755669f61b5d3b5dca59f27c52a4b4d3cdce460b
Vedat Gül <124357663+Vedatgul@users.noreply.github.com>
2023-09-02 13:14:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,5 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Grafiği oluşturma işlemini gerçekleştirin
-if st.button("Grafikleri Oluştur"):
-    # Verileri bir veri çerçevesine aktarın
-    data = {'Maaş': salaries}
-    df = pd.DataFrame(data)
-    
-    # Aynı maaşı alanları sayın
-    maas_sayisi = df['Maaş'].value_counts()
-    
-    # Grafiği oluşturun
-    st.bar_chart(maas_sayisi)
 
-    # Çubukların üzerine değerleri yazdırın
-    for i, v in enumerate(maas_sayisi):
-        st.text(f"{i}: {v} kişi")
+st.button("Grafikleri Oluştur")
